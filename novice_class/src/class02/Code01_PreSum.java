2914479d5b5668f41f5fcd2f93b4e3a27c04b0b0
xzp <xingzepeng@geo-compass.com>
2022-03-26 09:51:48 +0800

    排序

---
+++ novice_class/src/class02/Code01_PreSum.java
@@ -0,0 +1,19 @@
+package class02;
+
+public class Code01_PreSum {
+	
+	
+	public static class RangeSum1{
+	  private int arr [] ;
+	  
+	  public RangeSum1(int[] array) {
+		arr = array;
+	  }
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
