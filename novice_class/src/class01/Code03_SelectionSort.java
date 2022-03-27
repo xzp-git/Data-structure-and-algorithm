6c9d2f66d4acf067ba6613a87a3b6c17efcc2336
“xzpCSCKFWMS <l>
2022-03-27 22:41:54 +0800

    docs: 排序问题

--- novice_class/src/class01/Code03_SelectionSort.java
+++ novice_class/src/class01/Code03_SelectionSort.java
@@ -2,13 +2,28 @@ package class01;
 
 import java.util.Arrays;
 
-public class Code03_Sort {
+public class Code03_SelectionSort {
 	
-	public static void swap(int[] arr, int i , int j) {
+	public static void swap(int[] arr, int i, int j) {
 		int temp = arr[i];
-		arr[i] = arr[j];
+		arr[i] =arr[j];
 		arr[j] = temp;
 	}
+	
+	public static void selectionSort1(int[] arr) {
+		if(arr == null || arr.length < 2) {
+			return;
+		}
+		int N = arr.length;
+		for(int i = 0; i < N-1; i++) {
+			int minIndex = i;
+			for(int j = i + 1; j < N; j++) {
+				minIndex = arr[j] < arr[minIndex] ? j : minIndex ;
+			}
+			swap(arr, i, minIndex);
+		}
+	}
+	
 	
 	public static void selectionSort(int[] arr) {
 		if (arr == null || arr.length < 2) {
@@ -86,28 +101,28 @@ public class Code03_Sort {
 
 		// for test
 		public static void main(String[] args) {
-//			int testTime = 1;
-			int maxSize = 10;
+			int testTime = 100000;
+			int maxSize = 100;
 			int maxValue = 100;
-//			boolean succeed = true;
-//			for (int i = 0; i < testTime; i++) {
-//				int[] arr1 = generateRandomArray(maxSize, maxValue);
-//				int[] arr2 = copyArray(arr1);
-//				selectionSort(arr1);
-//				comparator(arr2);
-//				if (!isEqual(arr1, arr2)) {
-//					succeed = false;
-//					printArray(arr1);
-//					printArray(arr2);
-//					break;
-//				}
-//			}
-//			System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+			boolean succeed = true;
+			for (int i = 0; i < testTime; i++) {
+				int[] arr1 = generateRandomArray(maxSize, maxValue);
+				int[] arr2 = copyArray(arr1);
+				selectionSort1(arr1);
+				comparator(arr2);
+				if (!isEqual(arr1, arr2)) {
+					succeed = false;
+					printArray(arr1);
+					printArray(arr2);
+					break;
+				}
+			}
+			System.out.println(succeed ? "Nice!" : "Fucking fucked!");
 
-			int[] arr = generateRandomArray(maxSize, maxValue);
-			printArray(arr);
-			selectionSort(arr);
-			printArray(arr);
+//			int[] arr = generateRandomArray(maxSize, maxValue);
+//			printArray(arr);
+//			selectionSort1(arr);
+//			printArray(arr);
 		}
 
 }
